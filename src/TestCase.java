17888924c260402f1c078f96ee53363f34890895
Osman <muratcreek@gmail.com>
2019-09-18 16:07:44 -0500

    7. Commit

--- src/TestCase.java
+++ src/TestCase.java
@@ -6,6 +6,7 @@ public class TestCase {
         System.out.println("4.");
         System.out.println("5.");
         System.out.println("6.");
-        System.out.println("7");
+        System.out.println("7.");
+
     }
 }
